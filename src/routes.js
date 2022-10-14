8ddbd59303a448740eafd40d116941d367c5c40b
Victor Barrilhas <vbarrilhas@gmail.com>
2022-10-14 00:47:48 -0300

    feature: add authentication test

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 const routes = require("express").Router();
-const { User } = require("./app/models");
 
-User.create({
-  name: "Victor",
-  email: "victor@teste.com.br",
-  password_hash: "20032302",
-});
+routes.post('/sessions', (req, res) => {
+  
+})
 
 module.exports = routes;
