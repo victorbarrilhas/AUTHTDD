82a5f18a62863b2477bf18ed2adb6a898eaead71
Victor Barrilhas <vbarrilhas@gmail.com>
2022-10-14 12:54:03 -0300

    chore: add jwt

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -38,4 +38,19 @@ describe("Authentication", () => {
 
     expect(response.status).toBe(401);
   });
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await User.create({
+      name: "John",
+      email: "john@example.com",
+      password: "123123",
+    });
+
+    const response = await request(app).post("/sessions").send({
+      email: user.email,
+      password: "123123",
+    });
+
+    expect(response.body).toHaveProperty("token");
+  });
 });
